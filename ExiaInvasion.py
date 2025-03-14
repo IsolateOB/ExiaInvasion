3d69a019f618c2f161390329680f3f7c615bf4df
Tianyu Tan <tianyu.tan@student.unsw.edu.au>
2025-03-14 22:26:58 +1100

    1.0

--- ExiaInvasion.py
+++ ExiaInvasion.py
@@ -3,75 +3,90 @@ import json
 import requests
 import sys
 
-print("Please accept all cookies and log in with your account and password, do not use third-party login methods")
-print("请同意所有cookie并用账号密码完成登录, 不要用第三方登录方式")
 
-driver = webdriver.Edge()
-driver.get("https://www.blablalink.com/login")
+class ExiaInvasion:
+    def __init__(self):
+        print("Launching Edge browser")
+        print("正在启动Edge浏览器")
+        print()
+        print("请同意所有cookie并用账号密码完成登录, 不要用第三方登录方式")
 
-print()
-input("After logging in, press Enter to continue...\n 登录后按回车键继续...")
+        self.cookie_str = self.getCookies()
+        self.getPlayerNikkes_response = self.getPlayerNikkes()
 
-all_cookies = driver.get_cookies()
+    def getCookies(self):
+        important_keys = ["OptanonAlertBoxClosed",
+                          "game_login_game",
+                          "game_openid",
+                          "game_channelid",
+                          "game_token",
+                          "game_gameid",
+                          "game_user_name",
+                          "game_uid",
+                          "game_adult_status",
+                          "OptanonConsent"]
 
-important_keys = ["OptanonAlertBoxClosed",
-                  "game_login_game",
-                  "game_openid",
-                  "game_channelid",
-                  "game_token",
-                  "game_gameid",
-                  "game_user_name",
-                  "game_uid",
-                  "game_adult_status",
-                  "OptanonConsent"]
+        driver = webdriver.Edge()
+        driver.get("https://www.blablalink.com/login")
 
+        print()
+        input("After logging in, press Enter to continue...\n 登录后按回车键继续...")
 
-filtered_cookies = {cookie["name"]: cookie["value"] for cookie in all_cookies if cookie["name"] in important_keys}
-game_user_name = filtered_cookies["game_user_name"]
+        all_cookies = driver.get_cookies()
 
-for key, value in filtered_cookies.items():
-    if value is None:
-        print("Failed to get cookie, please close this window and run this program again")
-        print("Cookie获取失败，请关闭该窗口重新运行此程序")
-        sys.exit(1)
+        filtered_cookies = {cookie["name"]: cookie["value"] for cookie in all_cookies if
+                            cookie["name"] in important_keys}
 
-cookie_str = "; ".join([f"{key}={value}" for key, value in filtered_cookies.items()])
+        for key, value in filtered_cookies.items():
+            if value is None:
+                print("Failed to retrieve cookies. Please close this window and rerun the program.")
+                print("Cookie获取失败，请关闭该窗口重新运行此程序")
+                sys.exit(1)
 
-playerNikkeUrl = "https://api.blablalink.com/api/game/proxy/Tools/GetPlayerNikkes"
+        self.game_user_name = filtered_cookies["game_user_name"]
 
-headers = {
-    "Accept": "application/json, text/plain, */*",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "zh-CN,zh-TW;q=0.9,zh;q=0.8,en;q=0.7",
-    "Content-Length": "2",
-    "Content-Type" : "application/json",
-    "Cookie": cookie_str,
-    "Dnt": "1",
-    "Origin": "https://www.blablalink.com",
-    "priority": "u=1, i",
-    "Referer": "https://www.blablalink.com/",
-    "Sec-Ch-Ua": '"Chromium";v="134", "Not:A-Brand";v="24", "Microsoft Edge";v="134"',
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Ch-Ua-Platform": '"Windows"',
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "sec-Fetch-Site": "same-site",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0",
-    "x-channel-type": "2",
-    "x-common-params": '{"game_id":"16","area_id":"global","source":"pc_web","intl_game_id":"29080","language":"zh-TW","env":"prod","data_statistics_scene":"outer","data_statistics_page_id":"https://www.blablalink.com/","data_statistics_client_type":"pc_web","data_statistics_lang":"zh-TW"}',
-    "x-language": "zh-TW"
-}
+        cookie_str = "; ".join([f"{key}={value}" for key, value in filtered_cookies.items()])
 
-response = requests.post(playerNikkeUrl, headers=headers, json={})
+        return cookie_str
 
 
-filename = f"{game_user_name}.json"
-with open(filename, "w", encoding="utf-8") as f:
-    json.dump(response.json(), f, ensure_ascii=False, indent=4)
+    def getHeader(self, contentLength):
+        headers = {
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Language": "zh-CN,zh-TW;q=0.9,zh;q=0.8,en;q=0.7",
+            "Content-Length": contentLength,
+            "Content-Type": "application/json",
+            "Cookie": self.cookie_str,
+            "Dnt": "1",
+            "Origin": "https://www.blablalink.com",
+            "priority": "u=1, i",
+            "Referer": "https://www.blablalink.com/",
+            "Sec-Ch-Ua": '"Chromium";v="134", "Not:A-Brand";v="24", "Microsoft Edge";v="134"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": '"Windows"',
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "sec-Fetch-Site": "same-site",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0",
+            "x-channel-type": "2",
+            "x-common-params": '{"game_id":"16","area_id":"global","source":"pc_web","intl_game_id":"29080","language":"zh-TW","env":"prod","data_statistics_scene":"outer","data_statistics_page_id":"https://www.blablalink.com/","data_statistics_client_type":"pc_web","data_statistics_lang":"zh-TW"}',
+            "x-language": "zh-TW"
+        }
+        return headers
 
 
-print("Data has been saved to", filename)
-print(f"数据已保存到 {filename}")
-print("If it does not close automatically, you can close this window and the browser")
-print("若未自动关闭，可关闭此窗口与浏览器")
-sys.exit(0)
+    def getPlayerNikkes(self):
+        headers = self.getHeader(self.cookie_str, "2")
+        url = "https://api.blablalink.com/api/game/proxy/Tools/GetPlayerNikkes"
+        response = requests.post(url, headers=headers, json={})
+        return response
+
+    def writeJson(self, response):
+        game_user_name = self.game_user_name
+        filename = f"{game_user_name}.json"
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(response.json(), f, ensure_ascii=False, indent=4)
+
+        print(f"Data has been saved to {filename}")
+        print(f"数据已保存到 {filename}")
--- README.md
+++ README.md
@@ -1,21 +1,39 @@
 # ExiaInvasion
 
+`ExiaInvasion` is a crawler program that obtains Nikke character data in personal accounts from `blablalink.com` and generates tables
 
-
-## Introduction 简介
-
-A tool to get data from blablalink.
-
-一个用来从blablalink获取数据的工具
+`ExiaInvasion`是一个从`blablalink.com`获取个人账号中的Nikke角色数据并生成表格的爬虫程序
 
 
 
 ## Notice 注意
 
-- Use Edge browser by default
+- Make sure you have the `Edge` browser (installed by default on Windows systems)
 
-	默认使用Edge浏览器
+	确保你安装了`Edge`浏览器（Windows系统默认安装）
 
-- It may crash when you run it for the first time, please close it and run it again.
 
-	第一次运行可能崩溃，请关闭后重新运行
+- An error may be reported when running for the first time, please close it and run again.
+
+  第一次运行可能会报错，请关闭后重新运行
+
+
+
+## Usage 用法
+
+1. `SearchIndex` is used to provide the role search index of the main program `ExiaInvasion`. Roles can be added or deleted. The role name defaults to Simplified Chinese. The file can be changed as needed according to `nikke_list_en`
+
+	`SearchIndex`用于提供主程序`ExiaInvasion`的角色搜索索引，可添加或删除角色，角色名默认为简体中文。文件可对照`nikke_list_zh-TW`按需更改
+
+	
+
+2. `"character_ids"` consists of all 11 numbers between `id` and `id+10`
+
+	`"character_ids"`由`id`与`id+10`之间的所有11个数组成
+
+	
+
+3. `"priority"` is based on my personal subjective character priority, only for tabulation purposes, black>blue>yellow
+
+	`“priority”`是依据我个人主观的角色优先级，仅用于制表，黑>蓝>黄
+
