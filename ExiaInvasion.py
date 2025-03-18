de88fb3b74b241561d4d54fe0d4fecb30460162f
Tianyu Tan <tianyu.tan@student.unsw.edu.au>
2025-03-18 20:15:34 +1100

    增加了一些错误处理机制

--- ExiaInvasion.py
+++ ExiaInvasion.py
@@ -578,7 +578,7 @@ class ExiaInvasion:
 
 
 if __name__ == "__main__":
-    print("ExiaInvasion v1.22  by 灵乌未默")
+    print("ExiaInvasion v1.23  by 灵乌未默")
     print()
     print("GitHub:")
     print("github.com/IsolateOB/ExiaInvasion")
