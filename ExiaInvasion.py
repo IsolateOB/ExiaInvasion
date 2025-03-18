3dd3344bc1ced84fe5ea0048bdd2a113b01fdaa5
Tianyu Tan <tianyu.tan@student.unsw.edu.au>
2025-03-18 19:20:11 +1100

    更改相关描述

--- ExiaInvasion.py
+++ ExiaInvasion.py
@@ -580,6 +580,10 @@ if __name__ == "__main__":
     print("github.com/IsolateOB/ExiaInvasion")
     print()
 
+    print("The webpage may not open properly on the first run and may continuously report errors. Please close the program and restart it.")
+    print("第一次运行可能无法正常打开网页并连续报错，请关闭程序重新运行")
+    print()
+
     print("0: HK香港/MC澳门/TW台湾")
     print("1: JP日本/KR韩国/NA北美/SEA东南亚/Global全球")
     print()
