f379f1dbddfd8bc610600e855b480628b87523c1
Tianyu Tan <tianyu.tan@student.unsw.edu.au>
2025-04-25 15:38:17 +1000

    修改了版本号

--- ExiaInvasion.py
+++ ExiaInvasion.py
@@ -753,7 +753,7 @@ class ExiaInvasion:
 
 
 if __name__ == "__main__":
-    print("ExiaInvasion v1.54  by 灵乌未默")
+    print("ExiaInvasion v1.55  by 灵乌未默")
     print()
     print("GitHub:")
     print("github.com/IsolateOB/ExiaInvasion")
