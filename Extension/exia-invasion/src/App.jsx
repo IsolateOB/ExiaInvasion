e19faaf212e8efee38741cb6fa24c23dc6c18260
灵乌未默 <tigertan1998@gmail.com>
2025-05-26 17:56:14 +1000

    浏览器插件初始化

--- Extension/exia-invasion/src/App.jsx
+++ Extension/exia-invasion/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// src/App.jsx
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const handleClick = () => {
+    chrome.runtime.openOptionsPage()
+  };
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ width: 250, padding: 20 }}>
+      <button onClick={handleClick} style={{ fontSize: 18 }}>
+        {chrome.i18n.getMessage("set_accounts")}
+      </button>
+    </div>
+  );
 }
-
-export default App
+export default App;
